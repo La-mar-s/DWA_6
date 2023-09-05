cd3044d315c139574ff1b600ca12b41f0a230010
La-mar-s <sony.lamar.1995@gmail.com>
2023-09-05 12:32:21 +0200

    did some simple editing

--- scripts.js
+++ scripts.js
@@ -114,24 +114,18 @@ document
     const { theme } = Object.fromEntries(formData);
 
     if (theme === "night") {
-      document.documentElement.style.setProperty(
-        "--color-dark",
-        "255, 255, 255"
-      );
+      document.documentElement.style.setProperty("--color-dark","255, 255, 255");
       document.documentElement.style.setProperty("--color-light", "10, 10, 20");
     } else {
       document.documentElement.style.setProperty("--color-dark", "10, 10, 20");
-      document.documentElement.style.setProperty(
-        "--color-light",
-        "255, 255, 255"
-      );
+      document.documentElement.style.setProperty("--color-light","255, 255, 255");
     }
 
     document.querySelector("[data-settings-overlay]").open = false;
   });
 
   //try abstraction
-  
+
 document
   .querySelector("[data-search-form]")
   .addEventListener("submit", (event) => {
